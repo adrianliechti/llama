3ad7478db2f51d109c8be562263701330033d1f1
Adrian Liechti <adrian@monobox.ch>
2024-02-06 23:20:52 +0100

    add custom grpc plugins

---
+++ examples/custom-chat/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/adrianliechti/llama/pkg/provider/custom"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	l, err := net.Listen("tcp", ":50051")
+
+	if err != nil {
+		panic(err)
+	}
+
+	s := &Server{}
+
+	server := grpc.NewServer()
+
+	custom.RegisterCompleterServer(server, s)
+
+	if err := server.Serve(l); err != nil {
+		panic(err)
+	}
+}
+
+type Server struct {
+	custom.UnsafeCompleterServer
+}
+
+func (*Server) Complete(r *custom.CompletionRequest, stream custom.Completer_CompleteServer) error {
+	for _, m := range r.Messages {
+		println(m.Role.String() + ": " + m.Content)
+	}
+
+	println("")
+	println("")
+
+	last := r.Messages[len(r.Messages)-1]
+
+	completion := &custom.Completion{
+		Id: fmt.Sprintf("%d", time.Now().UnixMicro()),
+
+		Message: &custom.Message{
+			Role:    custom.Role_ROLE_ASSISTANT,
+			Content: last.Content,
+		},
+	}
+
+	return stream.Send(completion)
+}
--- go.mod
+++ go.mod
@@ -9,6 +9,8 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/sashabaranov/go-openai v1.19.1
 	github.com/stretchr/testify v1.8.4
+	google.golang.org/grpc v1.61.0
+	google.golang.org/protobuf v1.32.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
@@ -18,7 +20,10 @@ require (
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/oauth2 v0.16.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
-	google.golang.org/protobuf v1.32.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231106174013-bbf56f31fb17 // indirect
 )
