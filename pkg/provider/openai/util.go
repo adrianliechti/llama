244c74a42d073f3318b746e46a99227f22296aaf
Adrian Liechti <adrian@monobox.ch>
2024-10-07 21:53:18 +0200

    move voicebot example to own repo

--- pkg/provider/openai/util.go
+++ pkg/provider/openai/util.go
@@ -10,8 +10,8 @@ func convertError(err error) error {
 	var apierr *openai.Error
 
 	if errors.As(err, &apierr) {
-		println(string(apierr.DumpRequest(true)))  // Prints the serialized HTTP request
-		println(string(apierr.DumpResponse(true))) // Prints the serialized HTTP response
+		//println(string(apierr.DumpRequest(true)))  // Prints the serialized HTTP request
+		//println(string(apierr.DumpResponse(true))) // Prints the serialized HTTP response
 	}
 
 	return err
