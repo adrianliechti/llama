2c1df21789198ddebbf3bb3357c5697d344fc6aa
Adrian Liechti <adrian@monobox.ch>
2024-11-11 22:48:04 +0100

    wip

--- pkg/provider/google/embedder.go
+++ pkg/provider/google/embedder.go
@@ -1,91 +1,19 @@
 package google
 
 import (
-	"context"
-	"encoding/json"
-	"net/http"
-	"net/url"
-
-	"github.com/adrianliechti/llama/pkg/provider"
+	"github.com/adrianliechti/llama/pkg/provider/openai"
 )
 
-var _ provider.Embedder = (*Embedder)(nil)
+type Embedder = openai.Embedder
 
-type Embedder struct {
-	*Config
-}
+func NewEmbedder(model string, options ...Option) (*Embedder, error) {
+	url := "https://generativelanguage.googleapis.com/v1beta/"
 
-func NewEmbedder(url, model string, options ...Option) (*Embedder, error) {
-	cfg := &Config{
-		client: http.DefaultClient,
-
-		url:   "https://generativelanguage.googleapis.com",
-		model: model,
-	}
+	c := &Config{}
 
 	for _, option := range options {
-		option(cfg)
+		option(c)
 	}
 
-	return &Embedder{
-		Config: cfg,
-	}, nil
-}
-
-func (e *Embedder) Embed(ctx context.Context, content string) (*provider.Embedding, error) {
-	body := EmbedRequest{
-		Model: e.model,
-
-		Content: Content{
-			Parts: []ContentPart{
-				{
-					Text: content,
-				},
-			},
-		},
-	}
-
-	url, _ := url.JoinPath(e.url, "/v1beta/models/"+e.model+":embedContent")
-
-	if e.token != "" {
-		url += "?key=" + e.token
-	}
-
-	req, _ := http.NewRequestWithContext(ctx, "POST", url, jsonReader(body))
-	req.Header.Set("content-type", "application/json")
-
-	resp, err := e.client.Do(req)
-
-	if err != nil {
-		return nil, err
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, convertError(resp)
-	}
-
-	var result EmbedResponse
-
-	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-		return nil, err
-	}
-
-	return &provider.Embedding{
-		Data: result.Embedding.Values,
-	}, nil
-}
-
-type EmbedRequest struct {
-	Model   string  `json:"model"`
-	Content Content `json:"content"`
-}
-
-type EmbedResponse struct {
-	Embedding Embedding `json:"embedding"`
-}
-
-type Embedding struct {
-	Values []float32 `json:"values"`
+	return openai.NewEmbedder(url, model, c.options...)
 }
