1ac70df4cc1739ce9986f0de2910d26916e45ad7
Adrian Liechti <adrian@monobox.ch>
2024-12-04 15:03:50 +0100

    support client

--- pkg/provider/bedrock/config.go
+++ pkg/provider/bedrock/config.go
@@ -2,14 +2,6 @@ package bedrock
 
 type Config struct {
 	model string
-
-	region string
 }
 
 type Option func(*Config)
-
-func WithRegion(region string) Option {
-	return func(c *Config) {
-		c.region = region
-	}
-}
