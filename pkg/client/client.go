c86a173587f58384a795d8b6fb704b2c29691ca3
Adrian Liechti <adrian@monobox.ch>
2025-02-08 09:11:44 +0100

    wip

---
+++ pkg/client/client.go
@@ -0,0 +1,62 @@
+package client
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/openai/openai-go"
+	"github.com/openai/openai-go/option"
+)
+
+type Client struct {
+	Models *openai.ModelService
+
+	Embeddings  *openai.EmbeddingService
+	Completions *openai.ChatCompletionService
+
+	Segments    *SegmentService
+	Extractions *ExtractionService
+
+	Summaries *SummaryService
+}
+
+func New(url string, opts ...RequestOption) *Client {
+	opts = append(opts, WithURL(url))
+
+	return &Client{
+		Models: openai.NewModelService(openaiOptions(opts...)...),
+
+		Embeddings:  openai.NewEmbeddingService(openaiOptions(opts...)...),
+		Completions: openai.NewChatCompletionService(openaiOptions(opts...)...),
+
+		Segments:    NewSegmentService(opts...),
+		Extractions: NewExtractionService(opts...),
+	}
+}
+
+func newRequestConfig(opts ...RequestOption) *RequestConfig {
+	c := &RequestConfig{
+		Client: http.DefaultClient,
+	}
+
+	for _, opt := range opts {
+		opt(c)
+	}
+
+	return c
+}
+
+func openaiOptions(opts ...RequestOption) []option.RequestOption {
+	c := newRequestConfig(opts...)
+
+	options := []option.RequestOption{
+		option.WithHTTPClient(c.Client),
+		option.WithBaseURL(strings.TrimRight(c.URL, "/") + "/"),
+	}
+
+	if c.Token != "" {
+		options = append(options, option.WithAPIKey(c.Token))
+	}
+
+	return options
+}
